70c5efe7bcd60f8adc01b441c73bc102c6f806b3
hatem <hatem.houssein@gmail.com>
2019-06-29 22:38:03 +0900

    Restructring Graphql funcition and constants

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -4,39 +4,41 @@ import config from "../config.json";
 import products from "../models/products";
 import { findProduct } from "../utils";
 
-export default function() {
-  const typeDefs = `
-  scalar TimestampType
+// Normally I'd split this into 3 files; typeDefs.js, resolver.js and index.js.
+// But this is a small scale so no need to overkill it, I think
+const typeDefs = `
+scalar TimestampType
 
-  type Query {
-      products: [Product],
-      product(id: ID!): Product,
-  }
+type Query {
+    products: [Product],
+    product(id: ID!): Product,
+}
 
-  type Product {
-    id: ID!
-    transactions: [Transaction!] 
-  }
+type Product {
+  id: ID!
+  transactions: [Transaction!] 
+}
 
-  type Transaction {
-    id: ID!
-    quantity: Int
-    time: TimestampType
-  }
+type Transaction {
+  id: ID!
+  quantity: Int
+  time: TimestampType
+}
 `;
 
-  const resolvers = {
-    Query: {
-      products: () => products,
-      product: (_, { id }) => findProduct(id)
-    },
-    Product: {
-      // product transaction is an array in the "DB" but
-      // it's called `transactions` in the GraphQL Schema (not sure why)
-      transactions: product => product.transaction
-    }
-  };
+const resolvers = {
+  Query: {
+    products: () => products,
+    product: (_, { id }) => findProduct(id)
+  },
+  Product: {
+    // product transaction is an array in the "DB" but
+    // it's called `transactions` in the GraphQL Schema (not sure why)
+    transactions: product => product.transaction
+  }
+};
 
+export default function() {
   const graphQlServer = new GraphQLServer({ typeDefs, resolvers });
   const options = { port: process.env.GRAPHQL_PORT || config.graphQlPort };
   graphQlServer.start(options, () =>
