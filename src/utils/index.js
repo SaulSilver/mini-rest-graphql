7abe8a244b104e649ac6c04ea8ad648aaea34f40
hatem <hatem.houssein@gmail.com>
2019-06-30 18:48:18 +0900

    Find product by index helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,6 @@ import products from "../models/products";
 
 export const findProduct = id => products.find(product => product.id === id);
 
+export const findProdIndex = id => products.findIndex(product => product.id === id);
+
 export const randomize = max => Math.floor(Math.random() * Math.floor(max));
