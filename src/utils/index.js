aec211d6afe19395f94c269def1835ec7e5a00a6
hatem <hatem.houssein@gmail.com>
2019-06-30 15:54:12 +0900

    Helper randomizer for id, etc

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
 import products from "../models/products";
 
 export const findProduct = id => products.find(product => product.id === id);
+
+export const randomize = max => Math.floor(Math.random() * Math.floor(max));
