4cb44b5563a82739283d6539ee3f2b13ef3d0e98
hatem <hatem.houssein@gmail.com>
2019-06-30 15:54:36 +0900

    Create product GraphQL Mutation

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import TimestampType from "./timestamp_type";
 import config from "../config.json";
 import products from "../models/products";
-import { findProduct } from "../utils";
+import { findProduct, randomize } from "../utils";
 
 // Normally I'd split this into 3 files; typeDefs.js, resolver.js and index.js.
 // But this is a small scale so no need to overkill it, I think
@@ -24,6 +24,11 @@ type Transaction {
   quantity: Int
   time: TimestampType
 }
+
+type Mutation {
+  createProduct: Product!
+}
+
 `;
 
 const resolvers = {
@@ -35,6 +40,22 @@ const resolvers = {
     // product transaction is an array in the "DB" but
     // it's called `transactions` in the GraphQL Schema (not sure why)
     transactions: product => product.transaction
+  },
+  Mutation: {
+    createProduct: () => {
+      const newProd = {
+        id: randomize(600).toString(),
+        transaction: [
+          {
+            id: randomize(600).toString(),
+            quantity: randomize(100),
+            time: 1559328117100
+          }
+        ]
+      };
+      products.push(newProd);
+      return newProd;
+    }
   }
 };
 
